d7e071ab73240c5a6a255842f98947556aa3cf3c
Małgorzata Nowak <goskanowak87@gmail.com>
2019-06-24 17:06:16 +0200

    Add structure html

---
+++ script.js
@@ -0,0 +1,17 @@
+//======================= TO DO =============================
+//1. stworzenie inputa
+//2. pobranie wartości z inputa
+//3. przekazanie i stworzenie tyle kwadratów ile wskazał user
+//4. dodanie class i coloru do zaznaczenia
+//5. stworzenie buttona do losowania liczby
+//6. wylosowanie liczby z przedziału od 0 do wartości wskazanej przez usera;
+//7. user klika w kwadrat i sprawdzamy czy wybrana liczba to liczba wylosowana
+//8. sprawdzamy czy liczba wylowowana jest <> od liczby usera
+//9. podajemy komunikat ze liczba user jest <> od liczby wylosowanej
+//10. kolorujemy te które są <> od liczby;
+//11. kiedy user trafi liczbę pokazać komunikat że to jest właściwa liczba
+//12. podpowiedz, że w max 7 ruchach można trafić liczbę;
+//13. licznik kliknięć
+//14. wartość usera nie może być ujemna i mniejsza od 0
+//15. potwierdzenie add poprzez enter
+//16. instrukcja/ How to play
